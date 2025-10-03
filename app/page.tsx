b37550a6907f72008a02ce15c6672d6d8616d45c
Ashley Laisure <ashley.laisure@gmail.com>
2025-10-03 09:16:31 -0400

    bypassing authentication for now

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,18 @@
 import { Button } from "@/components/ui/button";
 import db from "@/lib/prisma";
+import Link from "next/link";
 
 export default async function Home() {
   const user = await db.user.findFirst()
 
   return (
     <>
-      <Button variant="default">Submit</Button>
-      <Button variant="destructive">Submit</Button>
-      <Button variant="ghost">Submit</Button>
-      <Button variant="link">Submit</Button>
-      <Button variant="outline">Submit</Button>
-      <Button variant="secondary">Submit</Button>
+      <Button variant="outline">
+        <Link href="/foods-management/foods">Admin View</Link>
+      </Button>
+      <Button variant="default" className="ml-4">
+        <Link href="/client">Client View</Link>
+      </Button>
     </>
   );
 }
---
+++ prisma/migrations/20251003131454_default_user/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Meal" ALTER COLUMN "userId" SET DEFAULT 1;
